40818af342946046ee71967b443e37b51180a17d
DaavidM <113436157+DaavidM9403@users.noreply.github.com>
2022-10-11 12:42:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Favorite Tv Program")
+print()
+
+# Question1
+tvShow = input("What is you favorite tv show?: ")
+# Main IF-Statement
+if tvShow == "Power":
+  print("Mines too")
+# Question 2  
+  favCharacter = input("Who is your Favorite Character?: ")
+  if favCharacter == "Kanan":
+    print("cool mines too")
+    
+  if favCharacter == "Ghost":
+    print("He was the main Character unitl his son killed him.")
+  
+  else:
+    print("They Alrihgt!")
+  
+elif tvShow == "Sistas":
+  print("Okay you get a pass my girlfriend is in to that show")
+else:
+  print("Okay Great")
